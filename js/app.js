855eb3072bd718b06c833ebfeeb9af0b8a5b9e08
Hosein <48409432+Ho3einWave@users.noreply.github.com>
2023-08-06 20:02:10 +0330

    swiper for industry

--- js/app.js
+++ js/app.js
@@ -9,3 +9,22 @@ document.querySelectorAll(".item--section3").forEach((el) => {
 });
 
 document.querySelector(".footer-year").textContent = new Date().getFullYear();
+
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    autoplay: true,
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+});
