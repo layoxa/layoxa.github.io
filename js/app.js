66a84cdf396d383209621e5460981443163387a4
Hosein <48409432+Ho3einWave@users.noreply.github.com>
2023-08-07 05:10:37 +0330

    lot of things

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 document.querySelectorAll(".item--section3").forEach((el) => {
     el.addEventListener("click", (e) => {
-        const clickedElement = event.target.closest(".item--section3");
+        const clickedElement = e.target.closest(".item--section3");
         document.querySelectorAll(".item--section3").forEach((rel) => {
             rel.classList.remove("active");
         });
+
+        document.querySelector(
+            ".img--section3"
+        ).src = `/img/${clickedElement.getAttribute("data-idx")}.png`;
         clickedElement.classList.add("active");
     });
 });
@@ -11,20 +15,15 @@ document.querySelectorAll(".item--section3").forEach((el) => {
 document.querySelector(".footer-year").textContent = new Date().getFullYear();
 
 const swiper = new Swiper(".swiper", {
-    // Optional parameters
     direction: "horizontal",
     loop: true,
     autoplay: true,
-    // If we need pagination
     pagination: {
         el: ".swiper-pagination",
     },
 
-    // Navigation arrows
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
-
-    // And if we need scrollbar
 });
